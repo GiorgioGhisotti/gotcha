e8fefed63987aa10c17df9182a5d216da72f29ac
GiorgioGhisotti <g.ghisotti@gmail.com>
2019-03-11 01:18:25 +0100

    yolo works, fixed incorrect cropping, added temporary dnn toggle switch

--- app/src/main/java/giorgioghisotti/unipr/it/gotcha/ImageEditor.kt
+++ app/src/main/java/giorgioghisotti/unipr/it/gotcha/ImageEditor.kt
@@ -1,6 +1,7 @@
 package giorgioghisotti.unipr.it.gotcha
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.graphics.Bitmap
@@ -24,6 +25,7 @@ import org.opencv.core.*
 import org.opencv.dnn.Dnn
 import org.opencv.dnn.Net
 import org.opencv.imgproc.Imgproc
+import org.opencv.utils.Converters
 import java.io.File
 import java.io.IOException
 
@@ -39,10 +41,6 @@ class ImageEditor : AppCompatActivity() {
     private var net: Net? = null
     private var busy: Boolean = false
     private val sDir = Environment.getExternalStorageDirectory().absolutePath
-    private val mobileNetSSDModelPath: String = "/Android/data/giorgioghisotti.unipr.it.gotcha/files/weights/MobileNetSSD/MobileNetSSD.caffemodel"
-    private val mobileNetSSDConfigPath: String = "/Android/data/giorgioghisotti.unipr.it.gotcha/files/weights/MobileNetSSD/MobileNetSSD.prototxt"
-    private val yoloV3ModelPath: String = "/Android/data/giorgioghisotti.unipr.it.gotcha/files/weights/YOLO/YOLOv3.weights"
-    private val yoloV3ConfigPath: String = "/Android/data/giorgioghisotti.unipr.it.gotcha/files/weights/YOLO/YOLOv3.cfg"
 
     // Initialize OpenCV manager.
     private val mLoaderCallback = object : BaseLoaderCallback(this) {
@@ -50,17 +48,29 @@ class ImageEditor : AppCompatActivity() {
             when (status) {
                 LoaderCallbackInterface.SUCCESS -> {
                     this@ImageEditor.genPreview()
-                    if (net == null) {
-                        val mobileSSDConfig = File(sDir + mobileNetSSDConfigPath)
-                        val mobileSSDModel = File(sDir + mobileNetSSDModelPath)
-                        val proto = mobileSSDConfig.absolutePath
-                        val weights = mobileSSDModel.absolutePath
-                        net = Dnn.readNetFromCaffe(proto, weights)
-                    }
+                    if (net == null) initializeNet()
                 }
                 else -> {
                     super.onManagerConnected(status)
                 }
+            }
+        }
+    }
+
+    private fun initializeNet() {
+        val path = sDir + "/" + resources.getString(R.string.weights_path)
+        val sharedPreferences = this.getSharedPreferences("dnn", Context.MODE_PRIVATE) ?: return
+        val dnn_type = sharedPreferences.getString("dnn_type", resources.getString(R.string.MobileNetSSD))
+        when (dnn_type) {
+            resources.getString(R.string.MobileNetSSD) -> {
+                val proto = File(path + resources.getString(R.string.MobileNetSSD_config_file)).absolutePath
+                val weights = File(path + resources.getString(R.string.MobileNetSSD_model_file)).absolutePath
+                net = Dnn.readNetFromCaffe(proto, weights)
+            }
+            resources.getString(R.string.YOLO) -> {
+                val cfg = File(path + resources.getString(R.string.YOLOv3_config_file)).absolutePath
+                val weights = File(path + resources.getString(R.string.YOLOv3_model_file)).absolutePath
+                net = Dnn.readNetFromDarknet(cfg, weights)
             }
         }
     }
@@ -84,11 +94,7 @@ class ImageEditor : AppCompatActivity() {
         mFindObjectButton!!.setOnClickListener {
             if(!busy) {
                 mFindObjectButton!!.isEnabled = false
-                try {
-                    NetProcessing(net, sourceImage)
-                } catch (e: OutOfMemoryError) {
-                    Toast.makeText(this, "The image is too large!", Toast.LENGTH_SHORT).show()
-                }
+                NetProcessing(net, sourceImage)
             }
         }
         if (mImageView != null) mImageView!!.setImageBitmap(imagePreview)
@@ -158,17 +164,28 @@ class ImageEditor : AppCompatActivity() {
         try{
             if(requestCode == RESULT_LOAD_IMG && resultCode == Activity.RESULT_OK){
                 if (data != null && data.data != null && mImageView != null) {
-                    sourceImage = MediaStore.Images.Media.getBitmap(
-                            this.contentResolver, data.data
-                    )
+                    currentImage = null
+                    sourceImage = null
+                    try {
+                        sourceImage = MediaStore.Images.Media.getBitmap(
+                                this.contentResolver, data.data
+                        )
+                    } catch (e: OutOfMemoryError) {
+                        Toast.makeText(this, "Sorry, this image is too big!", Toast.LENGTH_LONG).show()
+                    }
                     currentImage = sourceImage
                 } else return
 
                 genPreview()
             } else if(requestCode == RESULT_PICTURE && resultCode == Activity.RESULT_OK){
                 if (data != null && data.data!= null && mImageView != null) {
-                    val imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.data)
-                    sourceImage = imageBitmap
+                    sourceImage = null
+                    currentImage = null
+                    try {
+                        sourceImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.data)
+                    } catch (e: OutOfMemoryError) {
+                        Toast.makeText(this, "Sorry, this image is too big!", Toast.LENGTH_LONG).show()
+                    }
                     currentImage = sourceImage
 
                     genPreview()
@@ -208,6 +225,16 @@ class ImageEditor : AppCompatActivity() {
         }
     }
 
+    private fun getOutputNames(net : Net) : List<String>  {
+        val names : MutableList<String> = ArrayList()
+
+        val outLayers : List<Int> = net.unconnectedOutLayers.toList()
+        val layersNames : List<String> = net.getLayerNames()
+
+        outLayers.forEach{names.add(layersNames.get(it - 1))}
+        return names
+    }
+
     private fun NetProcessing(nnet: Net?, bbmp: Bitmap?) {
         val net: Net? = nnet
         var bmp: Bitmap? = bbmp
@@ -226,70 +253,141 @@ class ImageEditor : AppCompatActivity() {
                     this@ImageEditor.busy = false
                     return
                 }
-                this@ImageEditor.currentImage = null    //avoid filling the heap
-                val bmp32 = bmp!!.copy(Bitmap.Config.ARGB_8888, false)  //required format for bitmaptomat
+                this@ImageEditor.currentImage = null    //avoid filling the heap  //yes, I'm fighting the garbage collector
+                var bmp32: Bitmap? = null
+                try {
+                    bmp32 = bmp?.copy(Bitmap.Config.ARGB_8888, false)  //required format for bitmaptomat
+                } catch (e: OutOfMemoryError) {
+                    Toast.makeText(this@ImageEditor, "The image is too large!", Toast.LENGTH_LONG).show()
+                    this@ImageEditor.mFindObjectButton!!.isEnabled = true
+                    return
+                }
+                bmp = null    //avoid filling the heap
                 val frame = Mat(0,0,0)
                 bitmapToMat(bmp32, frame)
+                bmp32 = null    //avoid filling the heap
                 Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB)
 
-                val blob: Mat = Dnn.blobFromImage(frame, IN_SCALE_FACTOR,
-                        Size(IN_WIDTH.toDouble(), IN_HEIGHT.toDouble()),
-                        Scalar(MEAN_VAL, MEAN_VAL, MEAN_VAL), false)
-                net.setInput(blob)
-                var detections: Mat = net.forward()
-                var detections1: Mat = net.forward("detection_out")
-                var detections2: Mat = net.forward("conv17_2_mbox_priorbox")
-                blob.release()
-                val tot = detections.total().toInt()
-                val tot1 = detections1.total().toInt()
-                val tot2 = detections2.total().toInt()
-                detections2 = detections2.reshape(1, tot2)
-                for (i in 0..tot2-1) {
-                    val n: DoubleArray? = detections2.get(i, 0)
-                    println("Detections2, index " + i + ": " + n!![0])
-                }
-                detections = detections.reshape(1, tot / 7)
+                val sharedPreferences = this@ImageEditor.getSharedPreferences("dnn", Context.MODE_PRIVATE) ?: return
+                val dnn_type = sharedPreferences.getString("dnn_type", resources.getString(R.string.MobileNetSSD))
+                when (dnn_type) {
+                    resources.getString(R.string.MobileNetSSD) -> {
+                        val blob: Mat = Dnn.blobFromImage(frame, IN_SCALE_FACTOR_MNSSD,
+                                Size(IN_WIDTH_MNSSD.toDouble(), IN_HEIGHT_MNSSD.toDouble()),
+                                Scalar(MEAN_VAL_MNSSD, MEAN_VAL_MNSSD, MEAN_VAL_MNSSD), true, false)
+                        net.setInput(blob)
 
-                /**
-                 * Draw rectangles around detected objects (above a certain level of confidence)
-                 */
-                for (i in 0 until detections.rows()) {
-                    val confidence = detections.get(i, 2)[0]
-                    if (confidence > THRESHOLD) {
-                        println("Confidence: $confidence")
-                        val classId = detections.get(i, 1)[0].toInt()
-                        val xLeftBottom = (detections.get(i, 3)[0] * frame.cols()).toInt()
-                        val yLeftBottom = (detections.get(i, 4)[0] * frame.rows()).toInt()
-                        val xRightTop = (detections.get(i, 5)[0] * frame.cols()).toInt()
-                        val yRightTop = (detections.get(i, 6)[0] * frame.rows()).toInt()
+                        var detections: Mat = net.forward()
+                        blob.release()
+                        val tot = detections.total().toInt()
+                        detections = detections.reshape(1, tot / 7)
 
-                        Imgproc.rectangle(frame, Point(xLeftBottom.toDouble(), yLeftBottom.toDouble()),
-                                Point(xRightTop.toDouble(), yRightTop.toDouble()),
-                                Scalar(0.0, 255.0, 0.0), RECT_THICKNESS*scaleFactor(frame.cols()))
+                        /**
+                         * Draw rectangles around detected objects (above a certain level of confidence)
+                         */
+                        for (i in 0 until detections.rows()) {
+                            val confidence = detections.get(i, 2)[0]
+                            if (confidence > THRESHOLD_MNSSD) {
+                                println("Confidence: $confidence")
+                                val classId = detections.get(i, 1)[0].toInt()
+                                val xLeftBottom = (detections.get(i, 3)[0] * frame.cols()).toInt()
+                                val yLeftBottom = (detections.get(i, 4)[0] * frame.rows()).toInt()
+                                val xRightTop = (detections.get(i, 5)[0] * frame.cols()).toInt()
+                                val yRightTop = (detections.get(i, 6)[0] * frame.rows()).toInt()
 
-                        val label = classNames[classId] + ": " + String.format("%.2f", confidence)
-                        val baseLine = IntArray(1)
-                        val labelSize = Imgproc.getTextSize(label, Core.FONT_HERSHEY_SIMPLEX,
-                                FONT_SCALE.toDouble()*scaleFactor(frame.cols()),
-                                1, baseLine)
+                                Imgproc.rectangle(frame, Point(xLeftBottom.toDouble(), yLeftBottom.toDouble()),
+                                        Point(xRightTop.toDouble(), yRightTop.toDouble()),
+                                        Scalar(0.0, 255.0, 0.0), RECT_THICKNESS*scaleFactor(frame.cols()))
 
-                        Imgproc.rectangle(frame, Point(xLeftBottom.toDouble(), yLeftBottom - labelSize.height),
-                                Point(xLeftBottom + labelSize.width, (yLeftBottom + baseLine[0]).toDouble()),
-                                Scalar(0.0, 0.0, 0.0), Core.FILLED)
+                                val label = classNames[classId] + ": " + String.format("%.2f", confidence)
+                                val baseLine = IntArray(1)
+                                val labelSize = Imgproc.getTextSize(label, Core.FONT_HERSHEY_SIMPLEX,
+                                        FONT_SCALE.toDouble()*scaleFactor(frame.cols()),
+                                        1, baseLine)
 
-                        Imgproc.putText(frame, label,
-                                Point(xLeftBottom.toDouble(), yLeftBottom.toDouble()),
-                                Core.FONT_HERSHEY_SIMPLEX,
-                                FONT_SCALE.toDouble()*scaleFactor(frame.cols()),
-                                Scalar(255.0, 255.0, 255.0),
-                                FONT_THICKNESS*scaleFactor(frame.cols()))
+                                Imgproc.rectangle(frame, Point(xLeftBottom.toDouble(), yLeftBottom - labelSize.height),
+                                        Point(xLeftBottom + labelSize.width, (yLeftBottom + baseLine[0]).toDouble()),
+                                        Scalar(0.0, 0.0, 0.0), Core.FILLED)
 
+                                Imgproc.putText(frame, label,
+                                        Point(xLeftBottom.toDouble(), yLeftBottom.toDouble()),
+                                        Core.FONT_HERSHEY_SIMPLEX,
+                                        FONT_SCALE.toDouble()*scaleFactor(frame.cols()),
+                                        Scalar(255.0, 255.0, 255.0),
+                                        FONT_THICKNESS*scaleFactor(frame.cols()))
+                            }
+                        }
+                        detections.release()
+                    }
+                    resources.getString(R.string.YOLO) -> {
+                        val blob: Mat = Dnn.blobFromImage(frame, IN_SCALE_FACTOR_YOLO,
+                                Size(IN_WIDTH_YOLO.toDouble(), IN_HEIGHT_YOLO.toDouble()),
+                                Scalar(MEAN_VAL_YOLO, MEAN_VAL_YOLO, MEAN_VAL_YOLO), true, false)
+                        net.setInput(blob)
+                        val result: MutableList<Mat> = ArrayList()
+                        for (outname in getOutputNames(net)){
+                            result.add(net.forward(outname))
+                        }
+                        blob.release()
+
+                        val classIds : MutableList<Int> = ArrayList()
+                        val confs : MutableList<Float> = ArrayList()
+                        val rects : MutableList<Rect> = ArrayList()
+
+                        for (i in 0..result.size - 1){
+                            val level = result.get(i)
+                            for (j in 0..level.rows() - 1){
+                                val row = level.row(j)
+                                val scores = row.colRange(5, level.cols())
+                                val mm = Core.minMaxLoc(scores)
+                                val confidence = mm.maxVal.toFloat()
+                                val classIdPoint = mm.maxLoc
+                                if (confidence > THRESHOLD_YOLO){
+                                    val centerX = (row.get(0,0)[0] * frame.cols()).toInt()
+                                    val centerY = (row.get(0,1)[0] * frame.rows()).toInt()
+                                    val width   = (row.get(0,2)[0] * frame.cols()).toInt()
+                                    val height  = (row.get(0,3)[0] * frame.rows()).toInt()
+                                    val left    = centerX - width  / 2
+                                    val top     = centerY - height / 2
+
+
+                                    classIds.add(classIdPoint.x.toInt())
+                                    confs.add(confidence)
+                                    rects.add(Rect(left, top, width, height))
+                                }
+                            }
+                        }
+                        // Apply non-maximum suppression procedure.
+                        val nmsThresh = 0.4f
+                        val confidences = MatOfFloat(Converters.vector_float_to_Mat(confs))
+                        val boxesArray : Array<Rect> = rects.toTypedArray()
+                        val boxes = MatOfRect(*boxesArray)
+                        val indices = MatOfInt()
+                        Dnn.NMSBoxes(boxes, confidences, THRESHOLD_YOLO, nmsThresh, indices)
+
+                        // Draw result boxes:
+                        val ind = indices.toArray()
+                        for (i in 0..ind.size - 1){
+                            val idx = ind[i]
+                            val box = boxesArray[idx]
+                            Imgproc.rectangle(frame, box.tl(), box.br(),
+                                    Scalar(0.0,0.0,255.0),
+                                    RECT_THICKNESS*scaleFactor(frame.cols()))
+                        }
+                        for (mat in result){
+                            mat.release()
+                        }
                     }
                 }
-                bmp = Bitmap.createBitmap(frame.cols(), frame.rows(), Bitmap.Config.ARGB_8888)
+                try {
+                    bmp = Bitmap.createBitmap(frame.cols(), frame.rows(), Bitmap.Config.ARGB_8888)
+                } catch (e: OutOfMemoryError) {
+                    Toast.makeText(this@ImageEditor, "The image is too large!", Toast.LENGTH_LONG).show()
+                    this@ImageEditor.mFindObjectButton!!.isEnabled = true
+                    return
+                }
                 matToBitmap(frame, bmp)
                 frame.release()
-                detections.release()
                 this@ImageEditor.runOnUiThread(object: Runnable {
                     override fun run() {
                         this@ImageEditor.currentImage = bmp
@@ -305,12 +403,17 @@ class ImageEditor : AppCompatActivity() {
     private var metrics = DisplayMetrics()
 
     companion object {
-        private const val FONT_SCALE = 1.0.toFloat()
-        private const val THRESHOLD = 0.5
-        private const val IN_WIDTH = 300
-        private const val IN_HEIGHT = 300
-        private const val IN_SCALE_FACTOR = 0.007843
-        private const val MEAN_VAL = 127.5
+        private const val FONT_SCALE = 1.0f
+        private const val THRESHOLD_MNSSD = 0.5f
+        private const val THRESHOLD_YOLO = 0.5f
+        private const val IN_WIDTH_MNSSD = 300
+        private const val IN_HEIGHT_MNSSD = 300
+        private const val IN_WIDTH_YOLO = 416
+        private const val IN_HEIGHT_YOLO = 416
+        private const val IN_SCALE_FACTOR_MNSSD = 0.007843
+        private const val IN_SCALE_FACTOR_YOLO = 0.00392
+        private const val MEAN_VAL_MNSSD = 127.5
+        private const val MEAN_VAL_YOLO = 0.0
         private const val RECT_THICKNESS = 3
         private const val FONT_THICKNESS = 2
         private const val NORMAL_SIZE = 500
--- app/src/main/java/giorgioghisotti/unipr/it/gotcha/MainMenu.kt
+++ app/src/main/java/giorgioghisotti/unipr/it/gotcha/MainMenu.kt
@@ -5,7 +5,10 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.Switch
 
 class MainMenu : AppCompatActivity() {
 
@@ -26,5 +29,29 @@ class MainMenu : AppCompatActivity() {
             val mIntent = Intent(this, ImageEditor::class.java)
             this.startActivity(mIntent)
         }
+        val dnnToggleSwitch : Switch = findViewById(R.id.dnn_toggle)
+        val sharedPreferencesDnn = this.getSharedPreferences("dnn", Context.MODE_PRIVATE)
+        val dnn_type = sharedPreferences.getString("dnn_type", resources.getString(R.string.MobileNetSSD))
+        when (dnn_type) {
+            resources.getString(R.string.MobileNetSSD) -> {
+                dnnToggleSwitch.isChecked = false
+            }
+            resources.getString(R.string.YOLO) -> {
+                dnnToggleSwitch.isChecked = true
+            }
+        }
+        dnnToggleSwitch.textOff = "MobileNetSSD"
+        dnnToggleSwitch.textOn = "YOLOv3"
+        dnnToggleSwitch.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener(
+                fun(buttonView: CompoundButton, isChecked: Boolean) {
+                    val sharedPreferences = this@MainMenu.getSharedPreferences("dnn", Context.MODE_PRIVATE)
+                    if (isChecked) {
+                        sharedPreferences.edit().putString("dnn_type", resources.getString(R.string.YOLO)).apply()
+                    } else {
+                        sharedPreferences.edit().putString("dnn_type", resources.getString(R.string.MobileNetSSD)).apply()
+                    }
+                }
+        ))
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
